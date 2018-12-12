a5996c5a9fad373dc24eb74dee4f0ebd6e5c7027
anniesu1 <anniesumail@gmail.com>
2018-12-12 03:04:20 -0500

    fixed res.send errors

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -55,19 +55,23 @@ router.post('/create', (req, res) => {
       if (userRes.firstName === firstName && userRes.email === email) {
         Book.findOne({ 'title': title, 'author': author }, (existingBookErr, existingBook) => {
           if (existingBookErr) {
-            res.send({
-              success: false,
-              error: 'Error connecting to database',
-            });
-            return;
+            // res.send({
+            //   success: false,
+            //   error: 'Error connecting to database',
+            // });
+            res.setHeader('success', false);
+            res.setHeader('error', 'Error connecting to database');
+            //return;
           } else
           // Check if the book is already an existing book in the database
           if (existingBook) {
-            res.send({
-              success: false,
-              error: "Book already exists",
-            });
-            return;
+            // res.send({
+            //   success: false,
+            //   error: "Book already exists",
+            // });
+            res.setHeader('success', false);
+            res.setHeader('error', 'Error connecting to database');
+            //return;
           } else {
             // Create a new book
             const newBook = new Book({
@@ -79,106 +83,160 @@ router.post('/create', (req, res) => {
 
             newBook.save(function (saveBookErr, newBookDoc) {
               if (saveBookErr) {
-                res.send({
-                  success: false,
-                  error: 'Could not save book',
-                });
+                //res.send({
+                  // success: false,
+                  // error: 'Could not save book',
+                //});
+                res.setHeader('success', false);
+                res.setHeader('error', 'Error connecting to database');
               } else {
                 // Push to user's book collection
                 userRes.books.push(newBookDoc._id);
                 userRes.save(function (saveUserErr) {
                   if (saveUserErr) {
-                    res.send({
-                      success: false,
-                      error: saveUserErr
-                    });
+                    // res.send({
+                    //   success: false,
+                    //   error: saveUserErr
+                    // });
+                    res.setHeader('success', false);
+                    res.setHeader('error', 'Error connecting to database');
                   } else {
                     console.log("successfully bound the book to the user");
-                    res.send({
-                      success: true,
-                      error: null
-                    });
+                    // res.send({
+                    //   success: true,
+                    //   error: null
+                    // });
+                    res.setHeader('success', true);
+                    res.setHeader('error', null);
                   }
                 });
               }
             });
+            res.end();
           }
         });
       }
     } else {
-      res.redirect("http://localhost:3000/");
+      res.send({
+        success: false,
+        error: 'Cannot authenticate user'
+      })
     }
   });
 
-  // Attempt to find book in database
-  if (userId) {
-    Book.findOne({ 'title': title, 'author': author }, (existingBookErr, existingBook) => {
-      if (existingBookErr) {
-      	res.send({
-      		success: false,
-      		error: 'Error connecting to database',
-      	});
-        return;
-      } else
-    	// Check if the book is already an existing book in the database
-      if (existingBook) {
-        res.send({
-          success: false,
-          error: "Book already exists",
-        });
-        return;
-      } else {
-        // Create a new book
-        const newBook = new Book({
-          title: title,
-          author: author,
-          stars: stars,
-          reflections: reflections,
-        });
-
-        newBook.save(function (saveBookErr, newBookDoc) {
-          if (saveBookErr) {
-            res.send({
-              success: false,
-              error: 'Could not save book',
-            });
-          } else {
-            // Push to user's book collection
-            User.findById(userId, (findUserErr, findUserData) => {
-              if (findUserErr) {
-                res.send({
-                  success: false,
-                  error: 'Could not save book',
-                });
-              } else {
-                findUserData.books.push(newBookDoc._id);
-                findUserData.save(function (saveUserErr) {
-                  if (saveUserErr) {
-                    res.send({
-                      success: false,
-                      error: saveUserErr
-                    })
-                  } else {
-                    console.log("successfully bound the book to the user");
-                    res.send({
-                      success: true,
-                      error: null
-                    })
-                  }
-                });
-              }
-            });
-
-          }
-        });
-      }
-    });
-  } else {
-    // TODO: update the url accordingly after deployment
-    res.redirect("http://localhost:3000/");
-  }
+  // // Attempt to find book in database
+  // if (userId) {
+  //   Book.findOne({ 'title': title, 'author': author }, (existingBookErr, existingBook) => {
+  //     if (existingBookErr) {
+  //       res.setHeader('success', false);
+  //       res.setHeader('error', 'Error connecting to database');
+  //     	// res.send({
+  //     	// 	success: false,
+  //     	// 	error: 'Error connecting to database',
+  //     	// });
+  //       return;
+  //     } else
+  //   	// Check if the book is already an existing book in the database
+  //     if (existingBook) {
+  //       res.setHeader('success', false);
+  //       res.setHeader('error', 'Error connecting to database');
+  //       // res.send({
+  //       //   success: false,
+  //       //   error: "Book already exists",
+  //       // });
+  //       return;
+  //     } else {
+  //       // Create a new book
+  //       const newBook = new Book({
+  //         title: title,
+  //         author: author,
+  //         stars: stars,
+  //         reflections: reflections,
+  //       });
+  //
+  //       newBook.save(function (saveBookErr, newBookDoc) {
+  //         if (saveBookErr) {
+  //           // res.send({
+  //           //   success: false,
+  //           //   error: 'Could not save book',
+  //           // });
+  //           res.setHeader('success', false);
+  //           res.setHeader('error', 'Error connecting to database');
+  //         } else {
+  //           // Push to user's book collection
+  //           User.findById(userId, (findUserErr, findUserData) => {
+  //             if (findUserErr) {
+  //               // res.send({
+  //               //   success: false,
+  //               //   error: 'Could not save book',
+  //               // });
+  //               res.setHeader('success', false);
+  //               res.setHeader('error', 'Error connecting to database');
+  //             } else {
+  //               findUserData.books.push(newBookDoc._id);
+  //               findUserData.save(function (saveUserErr) {
+  //                 if (saveUserErr) {
+  //                   // res.send({
+  //                   //   success: false,
+  //                   //   error: saveUserErr
+  //                   // })
+  //                   res.setHeader('success', false);
+  //                   res.setHeader('error', 'Error connecting to database');
+  //                 } else {
+  //                   // res.send({
+  //                   //   success: true,
+  //                   //   error: null
+  //                   // })
+  //                   res.setHeader('success', true);
+  //                   res.setHeader('error', null);
+  //                 }
+  //               });
+  //             }
+  //           });
+  //         }
+  //       });
+  //     }
+  //     res.end();
+  //   });
+  // } else {
+  // }
 });
 
-
+// router.get('/getBooks', (req, res) => {
+//   var userId = jwt.decode(req.session.token, secret);
+//   var firstName = req.session.firstName;
+//   var email = req.session.email;
+//
+//   if (!reflections || !title || !author || !stars) {
+//     res.send({
+//       success: false,
+//       error: 'Error: Please fill out all form fields',
+//     });
+//     return;
+//   }
+//
+//   User.findById(userId, (err, userRes) => {
+//   }
+//
+//   // TODO: comment this all out
+//   User.findById("5c0fd7736677403188c65dd2")
+//     .populate('payments')
+//     .exec((error, user) => {
+//       if (error) {
+//         res.send({
+//           data: null,
+//           error: error
+//         });
+//         return;
+//       }
+//       console.log("user.payments is: " + user.payments);
+//       res.json({
+//         data: user.payments,
+//         error: null,
+//       });
+//       console.log("just sent shit to frontend");
+//       return;
+//     });
+// });
 
 module.exports = router;
